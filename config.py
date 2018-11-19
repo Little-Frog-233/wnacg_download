135b7fa902d0e08f89c58d2a6e6f55a76883f4bc
ruicheng <1342468180@qq.com>
2018-11-19 14:41:24 +0800

    配置文件

--- config.py
+++ config.py
@@ -2,5 +2,5 @@
 
 class wnacg_config:
 	def __init__(self):
-		self.save_path = '/Users/ruicheng/Downloads'
-		self.webdriver_path = '/Users/ruicheng/chromedriver01/chromedriver'
+		self.save_path = 'Downloads'
+		self.webdriver_path = 'chromedriver01/chromedriver'
